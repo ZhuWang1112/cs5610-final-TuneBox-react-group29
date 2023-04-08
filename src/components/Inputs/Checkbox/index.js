2c7e51bb72c4323930622df23410927f20a31bba
Zongyi000 <zongyijiang98@gmail.com>
2023-04-07 18:31:12 -0700

    login react initial

---
+++ src/components/Inputs/Checkbox/index.js
@@ -0,0 +1,24 @@
+
+
+
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import styles from "./styles.module.css";
+
+const CheckboxInput = ({ label, ...rest }) => {
+    return (
+        <FormControlLabel
+            className={styles.checkbox_container}
+            control={
+                <Checkbox
+                    {...rest}
+                    style={{ color: "#15883e" }}
+                    className={styles.checkbox}
+                />
+            }
+            label={label}
+        />
+    );
+};
+
+export default CheckboxInput;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from "react-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { ToastContainer } from "react-toastify";
+import store from "./redux/store";
+import './index.css';
+import "react-toastify/dist/ReactToastify.css";
+
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <PersistGate persistor={persistor}>
+                  <App />
+                  <ToastContainer
+                      position="bottom-center"
+                      autoClose={2000}
+                      hideProgressBar={true}
+                      closeButton={false}
+                      theme="colored"
+                      icon={false}
+                  />
+          </PersistGate>
+      </Provider>
   </React.StrictMode>
 );
 
