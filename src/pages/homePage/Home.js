34be35e8599cd1f52564767bdf5ff118fc89f38e
ZhuWang <wang.zhu1@northeastern.edu>
2023-04-14 17:58:13 -0700

    vip permission

--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -4,12 +4,14 @@ import {hotAlbumThunk, hotArtistThunk, hotPlaylistThunk, hotUserThunk} from '../
 import HomeCard from '../../components/HomeCard';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import {Link} from "react-router-dom";
+import UpgradeBanner from "../../components/UpgradeBanner";
 
 const Home = () => {
     const {artists} = useSelector((state) => state.hot);
     const {albums} = useSelector((state) => state.hot);
     const {playlists} = useSelector((state) => state.hot);
     const {users} = useSelector((state) => state.hot);
+    const {error} = useSelector((state) => state.hot);
 
     const dispatch = useDispatch();
 
@@ -41,38 +43,41 @@ const Home = () => {
     const displayAlbum = albums.slice(0, num);
     return (
         <div className={"m-2"}>
-            <div  className={"text-white"} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <h2 className={"mb-2"} style={{color: "gold"}}>Recommended Artists</h2>
-                {/*<p>current window width：{num}px</p>*/}
-                <div>
-                    <ArrowRightAltIcon />
-                    <Link to={"/hot/artist/all"} className={"m-3"}>View More</Link>
-                </div>
-            </div>
-
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {displayArtists.map((artist) => (
-                    <div key={artist.rank} style={{ flex: `1 0 ${100 / num}%`, maxWidth: `${100 / num}%` }}>
-                        <HomeCard item={artist} type={"artist"}/>
+            {error && <UpgradeBanner/>}
+            {!error && <div>
+                <div  className={"text-white"} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <h2 className={"mb-2"} style={{color: "gold"}}>Recommended Artists</h2>
+                    {/*<p>current window width：{num}px</p>*/}
+                    <div>
+                        <ArrowRightAltIcon />
+                        <Link to={"/hot/artist/all"} className={"m-3"}>View More</Link>
                     </div>
-                ))}
-            </div>
-
-            <div  className={"text-white mt-5"} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <h2 className={"mb-2"} style={{color: "gold"}}>Recommended Album</h2>
-                <div>
-                    <ArrowRightAltIcon />
-                    <Link to={"/hot/album/all"} className={"m-3"}>View More</Link>
                 </div>
-            </div>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {displayAlbum.map((album) => (
-                    <div key={album.rank} style={{ flex: `1 0 ${100 / num}%`, maxWidth: `${100 / num}%` }}>
-                        <HomeCard item={album} type={"album"}/>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {displayArtists.map((artist) => (
+                        <div key={artist.rank} style={{ flex: `1 0 ${100 / num}%`, maxWidth: `${100 / num}%` }}>
+                            <HomeCard item={artist} type={"artist"}/>
+                        </div>
+                    ))}
+                </div>
+
+                <div  className={"text-white mt-5"} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <h2 className={"mb-2"} style={{color: "gold"}}>Recommended Album</h2>
+                    <div>
+                        <ArrowRightAltIcon />
+                        <Link to={"/hot/album/all"} className={"m-3"}>View More</Link>
                     </div>
-                ))}
-            </div>
+                </div>
+
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {displayAlbum.map((album) => (
+                        <div key={album.rank} style={{ flex: `1 0 ${100 / num}%`, maxWidth: `${100 / num}%` }}>
+                            <HomeCard item={album} type={"album"}/>
+                        </div>
+                    ))}
+                </div>
+            </div>}
 
             <div  className={"text-white mt-5"} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <h2 className={"mb-2"} >Recommended Playlists</h2>
--- src/reducers/hot-reducer.js
+++ src/reducers/hot-reducer.js
@@ -3,21 +3,29 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const hotSlice = createSlice({
     name: "hot",
-    initialState: { artists: [], albums: [], playlists: [], users: [] },
+    initialState: { artists: [], albums: [], playlists: [], users: [], error: null },
     reducers: {},
     extraReducers: {
         [hotArtistThunk.fulfilled]: (state, { payload }) => {
             state.artists = payload;
+            state.error = null;
+        },
+        [hotArtistThunk.rejected]: (state, action) => {
+            state.error = action.error;
         },
         [hotAlbumThunk.fulfilled]: (state, { payload }) => {
             state.albums = payload;
+            state.error = null;
+        },
+        [hotAlbumThunk.rejected]: (state, action) => {
+            state.error = action.error;
         },
         [hotPlaylistThunk.fulfilled]: (state, { payload }) => {
             state.playlists = payload;
         },
         [hotUserThunk.fulfilled]: (state, { payload }) => {
             state.users = payload;
-        }
+        },
     },
 });
 
